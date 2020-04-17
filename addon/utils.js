5a096cf52d7515a3406fb3fadbe3e3dd9b557779
Patrick Collins <pat@burned.com>
2020-04-17 17:01:58 -0400

    Refactor with more tests
    - Remove `dependsOn`
    - Improve README

--- addon/utils.js
+++ addon/utils.js
@@ -1,3 +1,15 @@
+import { getOwner } from '@ember/application';
+import { assert } from '@ember/debug';
+import { typeOf } from '@ember/utils';
+import { guidFor } from '@ember/object/internals';
+const { keys } = Object;
+
+const SEPARATOR = '__';
+
+export function guidForConcern({ name, model }) {
+  return [name, guidFor(model)].join(SEPARATOR);
+}
+
 // Taken from `ember-concurrency`.
 export function _cleanupOnDestroy(owner, object, cleanupMethodName, ...args) {
   // TODO: find a non-mutate-y, non-hacky way of doing this.
@@ -24,4 +36,24 @@ export function _cleanupOnDestroy(owner, object, cleanupMethodName, ...args) {
   owner.willDestroy.__ember_processes_destroyers__.push(() => {
     object[cleanupMethodName](...args);
   });
+}
+
+export function getConcernFactory(parent, name) {
+  let owner = getOwner(parent);
+  assert('An owner is necessary to create a concern instance', owner);
+
+  let factory = owner.factoryFor(`concern:${name}`);
+  assert(`A concern with name '${name}' was not found`, factory);
+
+  return factory;
+}
+
+export function getPropertyNames(model) {
+  if (typeOf(model.eachAttribute) === 'function') {
+    let keys = [];
+    model.eachAttribute(key => keys.push(key));
+    return keys;
+  }
+
+  return keys(model);
 }
--- tests/unit/utils/utils-test.js
+++ tests/unit/utils/utils-test.js
@@ -1,8 +1,42 @@
 import * as utils from 'ember-concerns/utils';
 import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import EmberObject from '@ember/object';
 
 module('Unit | Utility | utils', function() {
-  test('it works', function(assert) {
-    assert.ok(typeof utils._cleanupOnDestroy === 'function');
+  module('_cleanupOnDestroy', function() {
+    test('it exists', function(assert) {
+      assert.ok(typeof utils._cleanupOnDestroy === 'function');
+    });
+  });
+
+  module('getConcernFactory', function(hooks) {
+    setupTest(hooks);
+
+    test('should throw when owner not found', function(assert) {
+      let obj = EmberObject.create();
+      assert.throws(() => {
+        utils.getConcernFactory(obj, 'bogusxyz');
+      }, /An owner is necessary/);
+    });
+
+    test('should throw when concern not found', function(assert) {
+      let obj = EmberObject.create(this.owner.ownerInjection());
+      assert.throws(() => {
+        utils.getConcernFactory(obj, 'bogusxyz');
+      }, /was not found/);
+    });
+
+    test('basic usage works as expected', function(assert) {
+      let obj = EmberObject.create(this.owner.ownerInjection());
+      let concern = utils.getConcernFactory(obj, 'test');
+      assert.ok(concern, 'concern should be found and initialized');
+    });
+  });
+
+  module('getPropertyNames', function() {
+    test('it exists', function(assert) {
+      assert.ok(typeof utils.getPropertyNames === 'function');
+    });
   });
 });
